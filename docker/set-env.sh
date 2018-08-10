4b3cb80a43454037c273a1da6e070a5d2689675f
Joshua Qiu <qiushaoxi@163.com>
2018-08-10 09:00:58 +0800

    Update set-env.sh

--- docker/set-env.sh
+++ docker/set-env.sh
@@ -1,6 +1,7 @@
 export eos_data_dir=`pwd`/../data
 export eos_config_dir=`pwd`/../config
-export docker_tag=qiushaoxi/eos-mainnet:v1.1.0
+export docker_tag=qiushaoxi/eos-mainnet:v1.1.4
 export container_name=nodeos
 export http_port=8888
 export p2p_port=9876
+export bnet_port=3389

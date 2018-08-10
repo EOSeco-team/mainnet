b477ef835ce6216aed1554d4348a2a77412c8475
qiushaoxi <qiushaoxi@163.com>
2018-08-10 09:28:47 +0800

    add bnet port

--- docker/hard-replay.sh
+++ docker/hard-replay.sh
@@ -6,7 +6,7 @@ docker rm -f $container_name
 
 docker run -ti --detach --name $container_name \
     -v $eos_config_dir:/etc/nodeos -v $eos_data_dir:/data \
-    -p $http_port:8888 -p $p2p_port:9876 \
+    -p $http_port:8888 -p $p2p_port:9876 -p $bnet_port:3389 \
     $docker_tag \
         /opt/eosio/bin/nodeos --data-dir=/data \
         --config-dir=/etc/nodeos \
